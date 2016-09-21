7b6d109778f0b9f5bd7712b1079ad26760e9d8ba
Mark Kennedy <mark@gamer-network.net>
2016-09-21 23:07:58 +0100

    trying to get gutbuster working with dynamically added rec branches

--- config_test.py
+++ config_test.py
@@ -2,52 +2,52 @@ OUTPUT_MODE="1080p30"
 CARD_MODE="1080p30"
 
 INPUTS=[
-	{
-		"name": "0",
-		"title": "0",
-		"src": {
-			"type": "test",
+    {
+        "name": "0",
+        "title": "0",
+        "src": {
+            "type": "test",
             "mode": CARD_MODE,
-		},
-	},
-	{
-		"name": "1",
-		"title": "1",
-		"src": {
-			"type": "test",
+        },
+    },
+    {
+        "name": "1",
+        "title": "1",
+        "src": {
+            "type": "test",
             "mode": CARD_MODE,
-		},
-	},
-	{
-		"name": "2",
-		"title": "2",
-		"src": {
-			"type": "test",
+        },
+    },
+    {
+        "name": "2",
+        "title": "2",
+        "src": {
+            "type": "test",
             "mode": CARD_MODE,
-		},
-	},
-	{
-		"name": "3",
-		"title": "3",
-		"src": {
-			"type": "test",
+        },
+    },
+    {
+        "name": "3",
+        "title": "3",
+        "src": {
+            "type": "test",
             "mode": CARD_MODE,
-		},
-	},
-	{
-		"name": "mix",
-		"title": "BROADCAST",
-		"src": {
-			"type": "test",
+        },
+    },
+    {
+        "name": "mix",
+        "title": "BROADCAST",
+        "src": {
+            "type": "test",
             "mode": CARD_MODE,
-		},
-	},
+        },
+    },
 ]
 
 LAYOUT=[
-	{ "input": "mix", "w": 1440, "h": 810, "x": 0, "y": 0 },
-	{ "input": "0", "w": 480, "h": 270, "x": 0, "y": 810 },
-	{ "input": "1", "w": 480, "h": 270 },
-	{ "input": "2", "w": 480, "h": 270 },
-	{ "input": "3", "w": 480, "h": 270 },
+    { "input": "mix", "w": 1440, "h": 810, "x": 0, "y": 0 },
+    { "input": "0", "w": 480, "h": 270, "x": 0, "y": 810 },
+    { "input": "1", "w": 480, "h": 270 },
+    { "input": "2", "w": 480, "h": 270 },
+    { "input": "3", "w": 480, "h": 270 },
 ]
